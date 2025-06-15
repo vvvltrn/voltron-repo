67f1fa68c8cc4dbe30afbf4c2453a841cf2e6b8d
vvvltrn <162269846+vvvltrn@users.noreply.github.com>
2025-06-15 19:29:06 -0400

    ys

---
+++ gaytest.py
@@ -0,0 +1,13 @@
+print("hello")
+print("this is the gay test")
+loop = 1
+while loop is 1:
+    answer = input("do you like men (y/n)")
+    if answer == "y":
+        print("thats pretty gay")
+        loop = 2
+    elif answer == "n":
+        print("thats pretty not gay")
+        loop = 3
+    else:
+        print("perdon?")
